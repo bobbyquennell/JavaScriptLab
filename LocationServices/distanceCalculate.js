15ba8a582f8e567a30979d5e864e2f4da548624f
bobby <bobbyquennell@gmail.com>
2014-12-22 14:36:59 +1100

    add distance calculation method

---
+++ LocationServices/distanceCalculate.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    var ourCoords = {
+    latitude: 47.624851,
+    longitude: -122.52099
+    }
+    var SamCoords = {
+        latitude: -37.8473,
+        longitude: 144.685
+    }
+    myCoords = {
+        latitude: -37.8473,
+        longitude: 144.685
+    }
+function computeDistance(startCoords, destCoords){
+    var startLatRads = degreesToRadians(startCoords.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+    var destLatRads = degreesToRadians(destCoords.latitude);
+    var destLongRads = degreesToRadians(destCoords.longitude);
+    
+    var Radius = 6371;//radius of the Earth in km.
+    var distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads) +
+                    Math.cos(startLatRads)*Math.cos(destLatRads) *
+                    Math.cos(startLongRads - destLongRads) ) * Radius;
+    return distance;
+
+}
+
+function degreesToRadians(degrees){
+    var radians = (degrees * Math.PI)/180;
+    return radians;
+}
--- LocationServices/getlocation.js
+++ LocationServices/getlocation.js
@@ -2,18 +2,38 @@
  * Created by LogicPlatypus on 22/12/2014.
  */
 window.onload=getlocation;
+
 function getlocation(position){
     if(navigator.geolocation)
     {
-        navigator.geolocation.getCurrentPosition(handleLocationInfo);
+        navigator.geolocation.getCurrentPosition(handleLocationInfo,errorhandler);
     }
     else{
         alert("Woops, we don't provide location service");
     }
 }
 function handleLocationInfo(position){
+
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     var div = document.getElementById("location");
     div.innerHTML = "your location is: latitude " + latitude + " longitude: " + longitude;
+
+    var km = computeDistance(position.coords, myCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + " km from the WickedSmart HQ";
+}
+function errorhandler(error){
+    var errorType = {
+        0:"Unknown error",
+        1:"Permission denied by user",
+        2:"Position is not available",
+        3:"Request timed out"
+    };
+    var errorMessage = errorType[error.code];
+    if(error.code == 0 || error.code == 2){
+        errorMessage += "" + error.message;
+    }
+    var div = document.getElementById("location");
+    div.innerHTML = errorMessage;
 }
